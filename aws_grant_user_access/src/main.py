c38da5f4f9c801358fcda0d74b35949d23e8dc2c
cob16 <public+github@cormacbrady.info>
2023-06-22 16:37:38 +0100

    PSEC-1886 check lambda input
    Co-Authored-By: j450nb <23558575+j450nb@users.noreply.github.com>

--- aws_grant_user_access/src/main.py
+++ aws_grant_user_access/src/main.py
@@ -1,4 +1,24 @@
+from jsonschema.validators import validate
+
+from aws_grant_user_access.src.policy_creator import PolicyCreator
+
+SCHEMA = {
+     "type" : "object",
+     "properties" : {
+         "username" :{"type" : "string", "description": "The AWS IAM username that will be granted access"},
+         "approval_in_hours" :{
+             "type" : "number",
+             "description": "a number of hours to approve access before it expires"
+         },
+     },
+    "required": ["username", "approval_in_hours"]
+ }
 
 
 def handle(event, context):
-    pass
+    iam_client = PolicyCreator()
+    process_event(event, iam_client)
+
+def process_event(event, iam_client):
+    validate(instance=event, schema=SCHEMA)
+    iam_client.grant_access(username='testuser', start_time='someimte now', endtime="sometime 12 hours later")
--- tests/unit/test_lambda_handler.py
+++ tests/unit/test_lambda_handler.py
@@ -1,5 +1,23 @@
-from aws_grant_user_access.src.main import handle
+from unittest.mock import Mock
+
+import pytest
+from jsonschema.exceptions import ValidationError
+
+from aws_grant_user_access.src.main import handle, process_event, PolicyCreator
 
 
-def test_handler_takes_events():
-    handle(dict(test=1), dict(context=1))
+def test_handler_takes_a_granted_event():
+    handle(dict(username="testuser", approval_in_hours=12), dict(context=1))
+
+def test_handler_rejects_invalid_events():
+    with pytest.raises(ValidationError):
+        handle(dict(username="testuser"), dict(context=1))
+
+def test_process_event_creates_iam_policy():
+    client = Mock(spec=PolicyCreator)
+    test_user = "testuser"
+    process_event(dict(username=test_user, approval_in_hours=12), iam_client=client)
+
+    client.grant_access.assert_called_with(username=test_user, start_time="someimte now", endtime="sometime 12 hours later")
+
+
