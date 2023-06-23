c9748b229d74f7b1cd4d309911e8d1f0dc57716d
j450nb <23558575+j450nb@users.noreply.github.com>
2023-06-23 16:10:04 +0100

    PSEC-1886 Patch policy creator in test

--- tests/unit/test_lambda_handler.py
+++ tests/unit/test_lambda_handler.py
@@ -1,14 +1,14 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import pytest
 from jsonschema.exceptions import ValidationError
 
 from aws_grant_user_access.src.main import handle, process_event, PolicyCreator
 
-TEST_ROLE_ARN = "arn:aws:iam::123456789012:role/somerole"
+TEST_ROLE_ARN = "arn:aws:iam::123456789012:role/RoleUserAccess"
 
-
-def test_handler_takes_a_granted_event():
+@patch("aws_grant_user_access.src.main.PolicyCreator")
+def test_handler_takes_a_granted_event(_mock_policy):
     handle(dict(role_arn=TEST_ROLE_ARN, username="testuser", approval_in_hours=12), dict(context=1))
 
 def test_handler_rejects_invalid_events():
@@ -17,9 +17,7 @@ def test_handler_rejects_invalid_events():
 
 def test_process_event_creates_iam_policy():
     client = Mock(spec=PolicyCreator)
-    test_user = "testuser"
+    test_user = "test-user"
     process_event(dict(role_arn=TEST_ROLE_ARN, username=test_user, approval_in_hours=12), iam_client=client)
 
     client.grant_access.assert_called_with(role_arn=TEST_ROLE_ARN, username=test_user, start_time="someimte now", end_time="sometime 12 hours later")
-
-
