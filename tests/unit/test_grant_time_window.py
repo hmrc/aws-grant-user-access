e9fe2c05c4a224bb92296144c343802fe7fba86a
j450nb <23558575+j450nb@users.noreply.github.com>
2023-06-26 10:25:20 +0100

    PSEC-1886: Uses freeze_time to test grant time window

--- tests/unit/test_grant_time_window.py
+++ tests/unit/test_grant_time_window.py
@@ -1,6 +1,8 @@
 from datetime import datetime
-
+from freezegun import freeze_time
 from aws_grant_user_access.src.grant_time_window import GrantTimeWindow
+
+@freeze_time('2023-06-21')
 def test_time_window():
     grant_time_window = GrantTimeWindow(hours=1)
     assert grant_time_window.start_time == datetime.utcnow()
