b1e14fb7efd89c3b59ebef0ed1fb990528b1f5bc
j450nb <23558575+j450nb@users.noreply.github.com>
2023-06-26 13:13:25 +0100

    PSEC-1886: Adds failing test to assert end_time

--- tests/unit/test_grant_time_window.py
+++ tests/unit/test_grant_time_window.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from freezegun import freeze_time
 from aws_grant_user_access.src.grant_time_window import GrantTimeWindow
 
@@ -6,3 +6,7 @@ from aws_grant_user_access.src.grant_time_window import GrantTimeWindow
 def test_time_window():
     grant_time_window = GrantTimeWindow(hours=1)
     assert grant_time_window.start_time == datetime.utcnow()
+
+def test_time_window_end_time():
+    grant_time_window = GrantTimeWindow(hours=1)
+    assert grant_time_window.end_time == datetime.utcnow() + timedelta(1)
