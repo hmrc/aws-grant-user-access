6830bef644b3636d1c95d303f4da1778ea32db46
j450nb <23558575+j450nb@users.noreply.github.com>
2023-06-26 13:43:30 +0100

    PSEC-1886: Fix failing test to assert end_time

--- aws_grant_user_access/src/grant_time_window.py
+++ aws_grant_user_access/src/grant_time_window.py
@@ -1,11 +1,15 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class GrantTimeWindow:
 
     def __init__(self, hours):
-        pass
+        self.hours = hours
 
     @property
     def start_time(self):
         return datetime.utcnow()
+
+
+    def end_time(self, start_time):
+        return start_time + timedelta(self.hours)
--- tests/unit/test_grant_time_window.py
+++ tests/unit/test_grant_time_window.py
@@ -7,6 +7,7 @@ def test_time_window():
     grant_time_window = GrantTimeWindow(hours=1)
     assert grant_time_window.start_time == datetime.utcnow()
 
+@freeze_time('2023-06-21')
 def test_time_window_end_time():
     grant_time_window = GrantTimeWindow(hours=1)
-    assert grant_time_window.end_time == datetime.utcnow() + timedelta(1)
+    assert grant_time_window.end_time(start_time=datetime.utcnow()) == datetime.utcnow() + timedelta(1)
