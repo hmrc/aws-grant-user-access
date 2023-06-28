42e27f3cc9a36c6e17f09c0f257a4e7297127f45
j450nb <23558575+j450nb@users.noreply.github.com>
2023-06-28 11:12:28 +0100

    PSEC-1886: Defines class for ExpiredPolicyCleaner

---
+++ aws_grant_user_access/src/expired_policy_cleaner.py
@@ -0,0 +1,3 @@
+class ExpiredPolicyCleaner:
+    def __init__(self):
+        pass
--- tests/unit/test_policy_cleanup.py
+++ tests/unit/test_policy_cleanup.py
@@ -1,2 +1,5 @@
+from aws_grant_user_access.src.expired_policy_cleaner import ExpiredPolicyCleaner
+
+
 def test_expired_policies_are_removed():
     expired_policy_cleaner = ExpiredPolicyCleaner()
