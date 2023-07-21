cc79d3e1d785a399308e7a61ef5d26078ed29ed9
wunzeco <1915636+wunzeco@users.noreply.github.com>
2023-07-21 11:29:13 +0100

    INFRA-7955 create ssm parameter store for lambda image tag

--- terraform/modules/lambda_grant_user_access/parameter_stores.tf
+++ terraform/modules/lambda_grant_user_access/parameter_stores.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "grant_user_access" {
-  name        = "${var.lambda_function_name}-image-tag"
+  name        = "/ecr/latest/${var.lambda_function_name}"
   description = "Latest ECR Image tag of ${var.lambda_function_name}"
   type        = "String"
   value       = "Initial value"
