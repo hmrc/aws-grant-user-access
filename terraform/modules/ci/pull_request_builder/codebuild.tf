81132777dd97a5660e7f218e7e6fd31d2582e705
wunzeco <1915636+wunzeco@users.noreply.github.com>
2023-07-21 06:36:09 +0100

    INFRA-7955 update codebuild compute type

--- terraform/modules/ci/pull_request_builder/codebuild.tf
+++ terraform/modules/ci/pull_request_builder/codebuild.tf
@@ -21,7 +21,7 @@ resource "aws_codebuild_project" "build" {
   }
 
   environment {
-    compute_type                = "BUILD_GENERAL1_LARGE"
+    compute_type                = "BUILD_GENERAL1_SMALL"
     image                       = "aws/codebuild/standard:6.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
