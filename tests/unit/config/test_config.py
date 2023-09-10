08be9a9b08febd5f214e1325e5174aea51544655
wunzeco <1915636+wunzeco@users.noreply.github.com>
2023-09-11 00:05:10 +0100

    PSEC-1933 add unit tests

--- tests/unit/config/test_config.py
+++ tests/unit/config/test_config.py
@@ -1,4 +1,3 @@
-
 from aws_grant_user_access.src.data.exceptions import InvalidConfigException
 import pytest
 from typing import Any
@@ -9,6 +8,7 @@ def test_get_default_log_level(monkeypatch: Any) -> None:
     monkeypatch.delenv("LOG_LEVEL", raising=False)
 
     assert Config.get_log_level() == "WARNING"
+
 
 def test_get_invalid_log_level(monkeypatch: Any) -> None:
     monkeypatch.setenv("LOG_LEVEL", "PANIC")
